d16443b1a6be8f166da0d791a4009f70994cdef6
chanyoung <chanchany0112@gmail.com>
2024-02-04 17:22:26 +0900

    [Add] adding aboutMe category

---
+++ _posts/2024-02-04-first-post.md
@@ -0,0 +1,12 @@
+---
+title: "github.io 블로그 첫 포스팅입니다."
+excerpt: "Github Blog 연습을 위해 써봅니다."
+
+categorise:
+  - Blog
+tags:
+  - Blog
+---
+### 앞으로 작성할 글에 대해서...
+앞으로 마크다운도 연습하면서, 그동안 노션에 적어두었던, 공부 내용들을 포스팅할 계획입니다. 
+블로그를 개설하고 포스팅할 때, 참고한 블로그 입니다 [참고 블로그](https://eona1301.github.io/categories/)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "About Me",
+        "excerpt":"저를 소개합니다.  이름 : 박찬영  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/aboutme/",
+        "teaser": null
+      },{
+        "title": "github.io 블로그 첫 포스팅입니다.",
+        "excerpt":"앞으로 작성할 글에 대해서…  앞으로 마크다운도 연습하면서, 그동안 노션에 적어두었던, 공부 내용들을 포스팅할 계획입니다.  블로그를 개설하고 포스팅할 때, 참고한 블로그 입니다 참고 블로그  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/first-post/",
+        "teaser": null
+      }]
